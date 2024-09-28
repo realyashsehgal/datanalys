4d618a4735e166cdcbe6900d40b4fac9360fd7fe
realyashsehgal <yash.sehgal1234@gmail.com>
2024-09-28 16:01:18 +0530

    coomit

--- merge and concate/concate.py
+++ merge and concate/concate.py
@@ -3,4 +3,10 @@ import numpy as np
 import sklearn.impute as SimpleImputer
 maindata = pd.read_csv('merge and concate/concmain.csv')
 print(maindata.head())
-print("Now we are going to concate our data")
+print("Now we are going to concate our data with some more values\n\n")
+
+new_data = pd.read_csv('merge and concate/conc1.csv')
+
+concated_data = pd.concat([maindata,new_data]).reset_index()
+print(concated_data.tail())
+print( concated_data.dtypes)
